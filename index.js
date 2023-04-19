cf959e958f7ea906335e18fb3c921e26baaa5dcb
vasanthmn1 <mnvasanth4@gmail.com>
2023-04-19 16:44:29 +0530

    change

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ DB()
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(cors({ origin: "*" }))
+// app.use(cors({ origin: "*" }))
+app.use(cors({
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200
+}));
+
 app.use(express.urlencoded({ extended: true }))
 app.use(usererrHandel)
 
