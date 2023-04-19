2de6456e10f0fd535b5ccec44cba55daa6a35add
vasanthmn1 <mnvasanth4@gmail.com>
2023-04-19 19:38:10 +0530

    ch

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ app.use(cors())
 app.use(cors(
     {
         origin: "http://localhost:5173",
-        credentials: true
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
     }
 ))
 // app.use(cors(a))
