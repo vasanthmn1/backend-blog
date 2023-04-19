789a385c86b85c6f49aed23c8cb110659c2880ad
vasanthmn1 <mnvasanth4@gmail.com>
2023-04-19 19:23:52 +0530

    push

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ DB()
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(cors({ origin: "*" }))
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+))
 // app.use(cors(a))
 
 app.use(express.urlencoded({ extended: true }))
