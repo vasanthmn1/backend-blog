0fca5002a3d7289d696e1ed3862bad259ed13de7
vasanthmn1 <mnvasanth4@gmail.com>
2023-04-21 10:56:54 +0530

    change

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json())
 app.use(cors())
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "https://master--spontaneous-mooncake-060ef3.netlify.app",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     }
